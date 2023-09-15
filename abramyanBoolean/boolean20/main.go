8bd8987e2e54413fcbb7d2ae9a37142b77ec3067
Qosim Qosimi <k.kosimi@yandex.com>
2023-09-15 10:46:57 +0500

    bool36-40 added, modif bool20

--- abramyanBoolean/boolean20/main.go
+++ abramyanBoolean/boolean20/main.go
@@ -13,11 +13,12 @@ func main() {
 	//Unfortunately this error caption does not catch "wrong type" error
 	//for example it reads normally these inputs: 514ijbg or 568.5642
 	_, err := fmt.Scan(&n)
+	//fmt.Println("JGJG: \t", n)
 	if err != nil {
 		fmt.Println("Input error: ", err)
 		return
-	} else if n > 999 || n < -999 {
-		fmt.Println("Трёхзначное число!")
+	} else if n/1000 != 0 || n/100 == 0 {
+		fmt.Println("Нужно трёхзначное число!")
 		return
 	}
 	if (n/100 != (n/10)%10) && ((n/10)%10 != n%10) && (n/100 != n%10) {
---
+++ abramyanBoolean/boolean36-40/main.go
@@ -0,0 +1,182 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x1, y1, x2, y2, chessPiece uint8
+	fmt.Println("Какая фигура сейчас на шахматной доске?")
+	fmt.Println("1 - Пешка\n2 - Ладья\n3 - Конь\n4 - Слон\n5 - Король\n6 - Ферзь")
+	_, err := fmt.Scan(&chessPiece)
+	if err != nil {
+		fmt.Print("Input error", err)
+		return
+	}
+	switch chessPiece {
+
+	case 1:
+		fmt.Print("Введите текущие координаты пешки: ")
+		_, err := fmt.Scan(&x1, &y1)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x1 == 0 || x1 > 8) && (y1 == 0 || y1 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		fmt.Print("Введите координаты будущей позиции пешки: ")
+		_, err = fmt.Scan(&x2, &y2)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x2 == 0 || x2 > 8) && (y2 == 0 || y2 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		if y2-y1 == 1 {
+			fmt.Print("Пешка может за один ход перейти на данную позицию")
+		} else if x1 == x2 {
+			fmt.Print("Пешка не может перейти на данную позицию")
+		} else {
+			fmt.Print("Пешка не может за один ход перейти на данную позицию")
+		}
+
+	case 2:
+		fmt.Print("Введите текущие координаты ладьи: ")
+		_, err := fmt.Scan(&x1, &y1)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x1 == 0 || x1 > 8) && (y1 == 0 || y1 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		fmt.Print("Введите координаты будущей позиции ладьи: ")
+		_, err = fmt.Scan(&x2, &y2)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x2 == 0 || x2 > 8) && (y2 == 0 || y2 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		if x1 == x2 || y1 == y2 {
+			fmt.Print("Ладья может за один ход перейти на данную позицию")
+		} else {
+			fmt.Print("Ладья не может за один ход перейти на данную позицию")
+		}
+
+	case 3:
+		fmt.Print("Введите текущие координаты коня: ")
+		_, err := fmt.Scan(&x1, &y1)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x1 == 0 || x1 > 8) && (y1 == 0 || y1 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		fmt.Print("Введите координаты будущей позиции коня: ")
+		_, err = fmt.Scan(&x2, &y2)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x2 == 0 || x2 > 8) && (y2 == 0 || y2 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		if (x1+2 == x2 && y1+1 == y2) || (x1+1 == x2 && y1+2 == y2) || (x1+2 == x2 && y1-1 == y2) || (x1+1 == x2 && y1-2 == y2) || (x1-2 == x2 && y1+1 == y2) || (x1-1 == x2 && y1+2 == y2) || (x1-2 == x2 && y1-1 == y2) || (x1-1 == x2 && y1-2 == y2) {
+			fmt.Print("Конь может за один ход перейти на данную позицию")
+		} else {
+			fmt.Print("Конь не может за один ход перейти на данную позицию")
+		}
+
+	case 4:
+		fmt.Print("Введите текущие координаты слона: ")
+		_, err := fmt.Scan(&x1, &y1)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x1 == 0 || x1 > 8) && (y1 == 0 || y1 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		fmt.Print("Введите координаты будущей позиции слона: ")
+		_, err = fmt.Scan(&x2, &y2)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x2 == 0 || x2 > 8) && (y2 == 0 || y2 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		if x2-x1 == y2-y1 || x1-x2 == y2-y1 {
+			fmt.Print("Слон может за один ход перейти на данную позицию")
+		} else if (x2-x1)%2 != (y2-y1)%2 || (x2-x1)%2 != (y1-y2)%2 {
+			fmt.Print("Слон не может перейти на данную позицию")
+		} else {
+			fmt.Print("Слон не может за один ход перейти на данную позицию")
+		}
+
+	case 5:
+		fmt.Print("Введите текущие координаты короля: ")
+		_, err := fmt.Scan(&x1, &y1)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x1 == 0 || x1 > 8) && (y1 == 0 || y1 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		fmt.Print("Введите координаты будущей позиции короля: ")
+		_, err = fmt.Scan(&x2, &y2)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x2 == 0 || x2 > 8) && (y2 == 0 || y2 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		if (x1 == x2 && y1+1 == y2) || (x1+1 == x2 && y1+1 == y2) || (x1+1 == x2 && y1 == y2) || (x1+1 == x2 && y1-1 == y2) || (x1 == x2 && y1-1 == y2) || (x1-1 == x2 && y1-1 == y2) || (x1-1 == x2 && y1 == y2) || (x1-1 == x2 && y1+1 == y2) {
+			fmt.Print("Король может за один ход перейти на данную позицию")
+		} else {
+			fmt.Print("Король не может за один ход перейти на данную позицию")
+		}
+
+	case 6:
+		fmt.Print("Введите текущие координаты ферзя: ")
+		_, err := fmt.Scan(&x1, &y1)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x1 == 0 || x1 > 8) && (y1 == 0 || y1 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		fmt.Print("Введите координаты будущей позиции ферзя: ")
+		_, err = fmt.Scan(&x2, &y2)
+		if err != nil {
+			fmt.Print("Input error", err)
+			return
+		}
+		if (x2 == 0 || x2 > 8) && (y2 == 0 || y2 > 8) {
+			fmt.Print("Таких координат нет!")
+			return
+		}
+		if (x2-x1 == y2-y1 || x1-x2 == y2-y1) || (x1 == x2 || y1 == y2) {
+			fmt.Print("Ферзь может за один ход перейти на данную позицию")
+		} else {
+			fmt.Print("Ферзь не может за один ход перейти на данную позицию")
+		}
+	}
+}
